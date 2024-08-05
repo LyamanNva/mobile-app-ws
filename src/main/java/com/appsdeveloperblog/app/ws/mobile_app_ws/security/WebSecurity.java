8227f2be80013893cbad706617921b6bafd1d79c
LyamanNva <lemannuriyeva006@gmail.com>
2024-08-05 21:17:38 +0400

    update WebSecurity

--- src/main/java/com/appsdeveloperblog/app/ws/mobile_app_ws/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobile_app_ws/security/WebSecurity.java
@@ -1,40 +1,40 @@
 package com.appsdeveloperblog.app.ws.mobile_app_ws.security;
 
-import com.appsdeveloperblog.app.ws.mobile_app_ws.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.appsdeveloperblog.app.ws.mobile_app_ws.service.UserService;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurity{
+public class WebSecurity {
 
     private final UserService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userService;
+    public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 
-        //Configure AuthenticationManagerBuilder
-        AuthenticationManagerBuilder authenticationManagerBuilder=
-                http.getSharedObject(AuthenticationManagerBuilder.class);
+        http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests(
+                (authz) -> authz.requestMatchers(HttpMethod.POST, "/users")
+                        .permitAll()
+                        .anyRequest().authenticated());
 
-        authenticationManagerBuilder.userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
-
-
-
-        http.csrf().disable()
-                .authorizeRequests().antMatchers(HttpMethod.POST,"/users").permitAll()
-                .anyRequest().authenticated();
         return http.build();
     }
+
 }
