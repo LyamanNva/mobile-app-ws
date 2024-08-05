0835302f6a2903b594d10a0437d12b979ef73e9c
LyamanNva <lemannuriyeva006@gmail.com>
2024-08-05 18:06:00 +0400

    add WebSecurity

--- src/main/java/com/appsdeveloperblog/app/ws/mobile_app_ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobile_app_ws/service/impl/UserServiceImpl.java
@@ -3,9 +3,13 @@ package com.appsdeveloperblog.app.ws.mobile_app_ws.service.impl;
 import com.appsdeveloperblog.app.ws.mobile_app_ws.UserRepository;
 import com.appsdeveloperblog.app.ws.mobile_app_ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.mobile_app_ws.service.UserService;
+import com.appsdeveloperblog.app.ws.mobile_app_ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.mobile_app_ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,20 +17,36 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    Utils utils;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public UserDto createUser(UserDto user) {
 
-        UserEntity userEntity=new UserEntity();
-        BeanUtils.copyProperties(user,userEntity);
+        if (userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
 
-        userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
 
-        UserEntity storedUserDetails=userRepository.save(userEntity);
+        String publicUserId=utils.generateUserId(30);
 
-        UserDto returnValue=new UserDto();
-        BeanUtils.copyProperties(storedUserDetails,returnValue);
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userEntity.setUserId(publicUserId);
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
 
         return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
